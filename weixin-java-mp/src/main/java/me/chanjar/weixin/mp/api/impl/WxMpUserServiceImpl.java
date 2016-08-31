5525c4fa27d50c2d588e8ecccb94a462e5fe115a
aimil <aimil@MiLin-PC>
2016-08-31 17:06:33 +0800

    添加批量查询用户基本信息功能，没有测试，没有添加超过100验证

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpUserServiceImpl.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpUserServiceImpl.java
@@ -1,5 +1,7 @@
 package me.chanjar.weixin.mp.api.impl;
 
+import java.util.List;
+
 import com.google.gson.JsonObject;
 
 import me.chanjar.weixin.common.exception.WxErrorException;
@@ -7,6 +9,7 @@ import me.chanjar.weixin.common.util.http.SimpleGetRequestExecutor;
 import me.chanjar.weixin.common.util.http.SimplePostRequestExecutor;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.api.WxMpUserService;
+import me.chanjar.weixin.mp.bean.WxMpUserQuery;
 import me.chanjar.weixin.mp.bean.result.WxMpUser;
 import me.chanjar.weixin.mp.bean.result.WxMpUserList;
 
@@ -14,35 +17,47 @@ import me.chanjar.weixin.mp.bean.result.WxMpUserList;
  * Created by Binary Wang on 2016/7/21.
  */
 public class WxMpUserServiceImpl implements WxMpUserService {
-  private static final String API_URL_PREFIX = "https://api.weixin.qq.com/cgi-bin/user";
-  private WxMpService wxMpService;
+	private static final String API_URL_PREFIX = "https://api.weixin.qq.com/cgi-bin/user";
+	private WxMpService wxMpService;
 
-  public WxMpUserServiceImpl(WxMpService wxMpService) {
-    this.wxMpService = wxMpService;
-  }
+	public WxMpUserServiceImpl(WxMpService wxMpService) {
+		this.wxMpService = wxMpService;
+	}
 
-  @Override
-  public void userUpdateRemark(String openid, String remark) throws WxErrorException {
-    String url = API_URL_PREFIX + "/info/updateremark";
-    JsonObject json = new JsonObject();
-    json.addProperty("openid", openid);
-    json.addProperty("remark", remark);
-    this.wxMpService.execute(new SimplePostRequestExecutor(), url, json.toString());
-  }
+	@Override
+	public void userUpdateRemark(String openid, String remark) throws WxErrorException {
+		String url = API_URL_PREFIX + "/info/updateremark";
+		JsonObject json = new JsonObject();
+		json.addProperty("openid", openid);
+		json.addProperty("remark", remark);
+		this.wxMpService.execute(new SimplePostRequestExecutor(), url, json.toString());
+	}
 
-  @Override
-  public WxMpUser userInfo(String openid, String lang) throws WxErrorException {
-    String url = API_URL_PREFIX + "/info";
-    lang = lang == null ? "zh_CN" : lang;
-    String responseContent = this.wxMpService.execute(new SimpleGetRequestExecutor(), url, "openid=" + openid + "&lang=" + lang);
-    return WxMpUser.fromJson(responseContent);
-  }
+	@Override
+	public WxMpUser userInfo(String openid, String lang) throws WxErrorException {
+		String url = API_URL_PREFIX + "/info";
+		lang = lang == null ? "zh_CN" : lang;
+		String responseContent = this.wxMpService.execute(new SimpleGetRequestExecutor(), url, "openid=" + openid + "&lang=" + lang);
+		return WxMpUser.fromJson(responseContent);
+	}
 
-  @Override
-  public WxMpUserList userList(String next_openid) throws WxErrorException {
-    String url = API_URL_PREFIX + "/get";
-    String responseContent = this.wxMpService.execute(new SimpleGetRequestExecutor(), url, next_openid == null ? null : "next_openid=" + next_openid);
-    return WxMpUserList.fromJson(responseContent);
-  }
+	@Override
+	public WxMpUserList userList(String next_openid) throws WxErrorException {
+		String url = API_URL_PREFIX + "/get";
+		String responseContent = this.wxMpService.execute(new SimpleGetRequestExecutor(), url, next_openid == null ? null : "next_openid=" + next_openid);
+		return WxMpUserList.fromJson(responseContent);
+	}
+
+	@Override
+	public List<WxMpUser> userInfoList(List<String> openidList) throws WxErrorException {
+		return userInfoList(new WxMpUserQuery(openidList));
+	}
+
+	@Override
+	public List<WxMpUser> userInfoList(WxMpUserQuery userQuery) throws WxErrorException {
+		String url = API_URL_PREFIX + "/info/batchget";
+		String responseContent = this.wxMpService.execute(new SimpleGetRequestExecutor(), url, userQuery.toJsonString());
+		return WxMpUser.fromJsonList(responseContent);
+	}
 
 }
--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/result/WxMpUser.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/bean/result/WxMpUser.java
@@ -1,143 +1,175 @@
 package me.chanjar.weixin.mp.bean.result;
 
-import me.chanjar.weixin.mp.util.json.WxMpGsonBuilder;
-
 import java.io.Serializable;
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import me.chanjar.weixin.mp.util.json.WxMpGsonBuilder;
 
 /**
  * 微信用户信息
+ * 
  * @author chanjarster
  *
  */
 public class WxMpUser implements Serializable {
 
-  protected Boolean subscribe;
-  protected String openId;
-  protected String nickname;
-  protected String sex;
-  protected String language;
-  protected String city;
-  protected String province;
-  protected String country;
-  protected String headImgUrl;
-  protected Long subscribeTime;
-  protected String unionId;
-  protected Integer sexId;
-  protected String remark;
-  protected Integer groupId;
+	/**
+	 * @fields serialVersionUID
+	 */
+	private static final long serialVersionUID = 5788154322646488738L;
+	protected Boolean subscribe;
+	protected String openId;
+	protected String nickname;
+	protected String sex;
+	protected String language;
+	protected String city;
+	protected String province;
+	protected String country;
+	protected String headImgUrl;
+	protected Long subscribeTime;
+	protected String unionId;
+	protected Integer sexId;
+	protected String remark;
+	protected Integer groupId;
 
-  public Boolean getSubscribe() {
-    return subscribe;
-  }
-  public Boolean isSubscribe() {
-    return subscribe;
-  }
-  public void setSubscribe(Boolean subscribe) {
-    this.subscribe = subscribe;
-  }
-  public String getOpenId() {
-    return openId;
-  }
-  public void setOpenId(String openId) {
-    this.openId = openId;
-  }
-  public String getNickname() {
-    return nickname;
-  }
-  public void setNickname(String nickname) {
-    this.nickname = nickname;
-  }
-  public String getSex() {
-    return sex;
-  }
-  public void setSex(String sex) {
-    this.sex = sex;
-  }
-  public String getLanguage() {
-    return language;
-  }
-  public void setLanguage(String language) {
-    this.language = language;
-  }
-  public String getCity() {
-    return city;
-  }
-  public void setCity(String city) {
-    this.city = city;
-  }
-  public String getProvince() {
-    return province;
-  }
-  public void setProvince(String province) {
-    this.province = province;
-  }
-  public String getCountry() {
-    return country;
-  }
-  public void setCountry(String country) {
-    this.country = country;
-  }
-  public String getHeadImgUrl() {
-    return headImgUrl;
-  }
-  public void setHeadImgUrl(String headImgUrl) {
-    this.headImgUrl = headImgUrl;
-  }
-  public Long getSubscribeTime() {
-    return subscribeTime;
-  }
-  public void setSubscribeTime(Long subscribeTime) {
-    this.subscribeTime = subscribeTime;
-  }
-  public String getUnionId() {
-    return unionId;
-  }
-  public void setUnionId(String unionId) {
-    this.unionId = unionId;
-  }
+	public Boolean getSubscribe() {
+		return subscribe;
+	}
 
-  public Integer getSexId() {
+	public Boolean isSubscribe() {
+		return subscribe;
+	}
 
-    return sexId;
-  }
+	public void setSubscribe(Boolean subscribe) {
+		this.subscribe = subscribe;
+	}
 
-  public void setSexId(Integer sexId) {
-    this.sexId = sexId;
-  }
+	public String getOpenId() {
+		return openId;
+	}
 
-  public String getRemark() {
-    return remark;
-  }
-  public void setRemark(String remark) {
-    this.remark = remark;
-  }
-  public Integer getGroupId() {
-    return groupId;
-  }
-  public void setGroupId(Integer groupId) {
-    this.groupId = groupId;
-  }
+	public void setOpenId(String openId) {
+		this.openId = openId;
+	}
 
-  public static WxMpUser fromJson(String json) {
-    return WxMpGsonBuilder.INSTANCE.create().fromJson(json, WxMpUser.class);
-  }
+	public String getNickname() {
+		return nickname;
+	}
 
-  @Override
-  public String toString() {
-    return "WxMpUser{" +
-        "subscribe=" + subscribe +
-        ", openId='" + openId + '\'' +
-        ", nickname='" + nickname + '\'' +
-        ", sex='" + sex + '\'' +
-        ", language='" + language + '\'' +
-        ", city='" + city + '\'' +
-        ", province='" + province + '\'' +
-        ", country='" + country + '\'' +
-        ", headImgUrl='" + headImgUrl + '\'' +
-        ", subscribeTime=" + subscribeTime +
-        ", unionId='" + unionId + '\'' +
-        ", remark='" + remark + '\'' +
-        ", groupId='" + groupId + '\'' +
-        '}';
-  }
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getHeadImgUrl() {
+		return headImgUrl;
+	}
+
+	public void setHeadImgUrl(String headImgUrl) {
+		this.headImgUrl = headImgUrl;
+	}
+
+	public Long getSubscribeTime() {
+		return subscribeTime;
+	}
+
+	public void setSubscribeTime(Long subscribeTime) {
+		this.subscribeTime = subscribeTime;
+	}
+
+	public String getUnionId() {
+		return unionId;
+	}
+
+	public void setUnionId(String unionId) {
+		this.unionId = unionId;
+	}
+
+	public Integer getSexId() {
+
+		return sexId;
+	}
+
+	public void setSexId(Integer sexId) {
+		this.sexId = sexId;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public Integer getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(Integer groupId) {
+		this.groupId = groupId;
+	}
+
+	public static WxMpUser fromJson(String json) {
+		return WxMpGsonBuilder.INSTANCE.create().fromJson(json, WxMpUser.class);
+	}
+
+	public static List<WxMpUser> fromJsonList(String json) {
+		Type collectionType = new TypeToken<List<WxMpUser>>() {
+		}.getType();
+		Gson gson = WxMpGsonBuilder.INSTANCE.create();
+		JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+		return gson.fromJson(jsonObject.get("user_info_list"), collectionType);
+	}
+
+	@Override
+	public String toString() {
+		return "WxMpUser{" + "subscribe=" + subscribe + ", openId='" + openId + '\'' + ", nickname='" + nickname + '\'' + ", sex='" + sex + '\'' + ", language='" + language + '\''
+				+ ", city='" + city + '\'' + ", province='" + province + '\'' + ", country='" + country + '\'' + ", headImgUrl='" + headImgUrl + '\'' + ", subscribeTime=" + subscribeTime
+				+ ", unionId='" + unionId + '\'' + ", remark='" + remark + '\'' + ", groupId='" + groupId + '\'' + '}';
+	}
 }
