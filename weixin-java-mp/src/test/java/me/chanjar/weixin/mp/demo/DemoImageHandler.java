8cbe2076673a84cdb6252c87fa10a1af04e7b2b8
Binary Wang <binarywang@gmail.com>
2016-10-26 15:33:37 +0800

    修复素材代码调整后的代码

--- weixin-java-mp/src/test/java/me/chanjar/weixin/mp/demo/DemoImageHandler.java
+++ weixin-java-mp/src/test/java/me/chanjar/weixin/mp/demo/DemoImageHandler.java
@@ -10,7 +10,6 @@ import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutImageMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 
-import java.io.IOException;
 import java.util.Map;
 
 public class DemoImageHandler implements WxMpMessageHandler {
@@ -30,9 +29,8 @@ public class DemoImageHandler implements WxMpMessageHandler {
       return m;
     } catch (WxErrorException e) {
       e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
     }
+    
     return null;
   }
 }
