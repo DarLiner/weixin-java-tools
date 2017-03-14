b8c8d105796a39e62fe671d24c23acebf8dfd020
aimilin6688 <aimilin@yeah.net>
2017-03-14 11:53:25 +0800

    增加扫码支付回掉结果Bean

---
+++ weixin-java-pay/src/main/java/com/github/binarywang/wxpay/bean/result/WxScanPayNotifyResult.java
@@ -0,0 +1,24 @@
+package com.github.binarywang.wxpay.bean.result;
+
+import java.io.Serializable;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+public class WxScanPayNotifyResult  extends WxPayBaseResult implements Serializable{
+  private static final long serialVersionUID = 3381324564266118986L;
+  
+  /**
+   * 预支付ID
+   */
+  @XStreamAlias("prepay_id")
+  private String prepayId;
+
+  public String getPrepayId() {
+    return prepayId;
+  }
+
+  public void setPrepayId(String prepayId) {
+    this.prepayId = prepayId;
+  }
+  
+}
