16dea387f2f9a2941876f9f80c7bba6af8d2187d
ecoolper <crskyp@163.com>
2017-05-19 23:48:47 +0800

    修改两个文件

--- weixin-java-mp/src/main/java/me/chanjar/weixin/mp/util/http/MaterialVideoInfoRequestExecutor.java
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/util/http/MaterialVideoInfoRequestExecutor.java
@@ -1,25 +1,33 @@
 package me.chanjar.weixin.mp.util.http;
 
-import me.chanjar.weixin.common.util.http.RequestExecutor;
-import me.chanjar.weixin.common.util.http.RequestHttp;
-import me.chanjar.weixin.mp.bean.material.WxMpMaterialVideoInfoResult;
-import me.chanjar.weixin.mp.util.http.apache.ApacheMaterialVideoInfoRequestExecutor;
-import me.chanjar.weixin.mp.util.http.jodd.JoddMaterialVideoInfoRequestExecutor;
-import me.chanjar.weixin.mp.util.http.okhttp.OkhttpMaterialVideoInfoRequestExecutor;
+
+  import me.chanjar.weixin.common.util.http.RequestExecutor;
+  import me.chanjar.weixin.common.util.http.RequestHttp;
+
+  import me.chanjar.weixin.mp.bean.material.WxMpMaterialVideoInfoResult;
+  import me.chanjar.weixin.mp.util.http.apache.ApacheMaterialVideoInfoRequestExecutor;
+  import me.chanjar.weixin.mp.util.http.jodd.JoddMaterialVideoInfoRequestExecutor;
+  import me.chanjar.weixin.mp.util.http.okhttp.OkhttpMaterialVideoInfoRequestExecutor;
+
 
 public abstract class MaterialVideoInfoRequestExecutor<H, P> implements RequestExecutor<WxMpMaterialVideoInfoResult, String> {
   protected RequestHttp<H, P> requestHttp;
 
   public MaterialVideoInfoRequestExecutor(RequestHttp requestHttp) {
-    this.requestHttp = requestHttp;
-  }
+          this.requestHttp = requestHttp;
+        }
 
-  @Override
-  public WxMpMaterialVideoInfoResult executeJodd(HttpConnectionProvider provider, ProxyInfo proxyInfo, String uri, String materialId) throws WxErrorException, IOException {
-    HttpRequest request = HttpRequest.post(uri);
-    if (proxyInfo != null) {
-      provider.useProxy(proxyInfo);
-    }
-  }
+        public static RequestExecutor<WxMpMaterialVideoInfoResult, String> create(RequestHttp requestHttp) {
+          switch (requestHttp.getRequestType()) {
+            case apacheHttp:
+              return new ApacheMaterialVideoInfoRequestExecutor(requestHttp);
+            case joddHttp:
+              return new JoddMaterialVideoInfoRequestExecutor(requestHttp);
+            case okHttp:
+              return new OkhttpMaterialVideoInfoRequestExecutor(requestHttp);
+            default:
+              return null;
+          }
+        }
 
-}
+      }
