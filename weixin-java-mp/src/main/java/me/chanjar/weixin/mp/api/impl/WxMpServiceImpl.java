c0172785a747ea26262a6c31eca3d6f5f56141ca
Binary Wang <binarywang@gmail.com>
2017-05-27 10:21:58 +0800

    增加默认接口实现类，兼容之前版本

---
+++ weixin-java-mp/src/main/java/me/chanjar/weixin/mp/api/impl/WxMpServiceImpl.java
@@ -0,0 +1,11 @@
+package me.chanjar.weixin.mp.api.impl;
+
+/**
+ * <pre>
+ * 默认接口实现类，使用apache httpclient实现
+ * Created by Binary Wang on 2017-5-27.
+ * @author <a href="https://github.com/binarywang">binarywang(Binary Wang)</a>
+ * </pre>
+ */
+public class WxMpServiceImpl extends me.chanjar.weixin.mp.api.impl.apache.WxMpServiceImpl{
+}
