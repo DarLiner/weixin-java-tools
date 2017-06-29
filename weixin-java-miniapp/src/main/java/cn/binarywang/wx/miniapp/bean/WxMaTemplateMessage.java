d6bb355cd10fbcd029f6212042fc4c3bd678ed4c
Binary Wang <binarywang@gmail.com>
2017-06-29 20:07:50 +0800

    #260 修复小程序发送模版消息接口的问题，并重新整理javadoc

--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/WxMaTemplateMessage.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/bean/WxMaTemplateMessage.java
@@ -13,34 +13,67 @@ import java.util.List;
  */
 public class WxMaTemplateMessage implements Serializable {
   private static final long serialVersionUID = 5063374783759519418L;
-  /**
-   * touser	是	接收者（用户）的 openid
-   */
-  private String toUser;
-  /**
-   * template_id	是	所需下发的模板消息的id
-   */
-  private String templateId;
+
   /**
    * <pre>
-   * page	否	点击模板卡片后的跳转页面，仅限本小程序内的页面。支持带参数,（示例index?foo=bar）。该字段不填则模板无跳转。
+   * 参数：touser
+   * 是否必填： 是
+   * 描述： 接收者（用户）的 openid
+   * </pre>
+   */
+  private String toUser;
+
+  /**
+   * <pre>
+   * 参数：template_id
+   * 是否必填： 是
+   * 描述： 所需下发的模板消息的id
+   * </pre>
+   */
+  private String templateId;
+
+  /**
+   * <pre>
+   * 参数：page
+   * 是否必填： 否
+   * 描述： 点击模板卡片后的跳转页面，仅限本小程序内的页面。支持带参数,（示例index?foo=bar）。该字段不填则模板无跳转。
    * </pre>
    */
   private String page;
+
   /**
-   * form_id	是	表单提交场景下，为 submit 事件带上的 formId；支付场景下，为本次支付的 prepay_id
+   * <pre>
+   * 参数：form_id
+   * 是否必填： 是
+   * 描述： 表单提交场景下，为 submit 事件带上的 formId；支付场景下，为本次支付的 prepay_id
+   * </pre>
    */
   private String formId;
+
   /**
-   * data	是	模板内容，不填则下发空模板
+   * <pre>
+   * 参数：data
+   * 是否必填： 是
+   * 描述： 模板内容，不填则下发空模板
+   * </pre>
    */
   private List<Data> data = new ArrayList<>();
+
   /**
-   * color	否	模板内容字体的颜色，不填默认黑色
+   * <pre>
+   * 参数：color
+   * 是否必填： 否
+   * 描述： 模板内容字体的颜色，不填默认黑色
+   * </pre>
    */
   private String color;
+
   /**
-   * emphasis_keyword	否	模板需要放大的关键词，不填则默认无放大
+   * <pre>
+   * 参数：emphasis_keyword
+   * 是否必填： 否
+   * 描述： 模板需要放大的关键词，不填则默认无放大
+   * </pre>
    */
   private String emphasisKeyword;
 
