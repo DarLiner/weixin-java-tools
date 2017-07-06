368dbc97218960139d9d1d05433178784a9ff7f6
Binary Wang <binarywang@gmail.com>
2017-07-06 15:44:58 +0800

    fix javadoc

--- weixin-java-common/src/main/java/me/chanjar/weixin/common/annotation/Required.java
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/annotation/Required.java
@@ -6,9 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <pre>
  * 标识某个字段是否是必填的
- * <p>
  * Created by Binary Wang on 2016/9/25.
+ * </pre>
  *
  * @author binarywang (https://github.com/binarywang)
  */
