8a31c188ca9c09f4278634c530759f360e185160
Binary Wang <binarywang@gmail.com>
2017-10-26 18:57:39 +0800

    修复单元测试

--- weixin-java-mp/src/test/java/me/chanjar/weixin/mp/api/impl/WxMpStoreServiceImplTest.java
+++ weixin-java-mp/src/test/java/me/chanjar/weixin/mp/api/impl/WxMpStoreServiceImplTest.java
@@ -30,10 +30,17 @@ public class WxMpStoreServiceImplTest {
    */
   public void testAdd() throws WxErrorException {
     this.wxMpService.getStoreService()
-      .add(WxMpStoreBaseInfo.builder().businessName("haha").branchName("abc")
-        .province("aaa").district("aaa").telephone("122").address("abc").categories(new String[]{"美食,江浙菜"})
+      .add(WxMpStoreBaseInfo.builder()
+        .businessName("haha")
+        .branchName("abc")
+        .province("aaa")
+        .district("aaa")
+        .telephone("122")
+        .address("abc")
+        .categories(new String[]{"美食,江浙菜"})
         .longitude(new BigDecimal("115.32375"))
-        .latitude(new BigDecimal("25.097486")).city("aaa").offsetType(1)
+        .latitude(new BigDecimal("25.097486"))
+        .city("aaa")
         .build());
     // 以下运行会抛异常
     this.wxMpService.getStoreService().add(WxMpStoreBaseInfo.builder().build());
@@ -41,9 +48,14 @@ public class WxMpStoreServiceImplTest {
 
   public void testUpdate() throws WxErrorException {
     this.wxMpService.getStoreService()
-      .update(WxMpStoreBaseInfo.builder().poiId("291503654").telephone("020-12345678")
-        .sid("aaa").avgPrice(35).openTime("8:00-20:00").special("免费wifi，外卖服务")
-        .introduction("麦当劳是全球大型跨国连锁餐厅，1940 年创立于美国，在世界上大约拥有3 万间分店。主要售卖汉堡包，以及薯条、炸鸡、汽水、冰品、沙拉、水果等快餐食品").offsetType(1)
+      .update(WxMpStoreBaseInfo.builder()
+        .poiId("291503654")
+        .telephone("020-12345678")
+        .sid("aaa")
+        .avgPrice(35)
+        .openTime("8:00-20:00")
+        .special("免费wifi，外卖服务")
+        .introduction("麦当劳是全球大型跨国连锁餐厅，1940 年创立于美国，在世界上大约拥有3 万间分店。主要售卖汉堡包，以及薯条、炸鸡、汽水、冰品、沙拉、水果等快餐食品")
         .build());
   }
 
