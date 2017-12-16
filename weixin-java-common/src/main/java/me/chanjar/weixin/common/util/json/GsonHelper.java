2e85dfdf7a3faa5894e8d5ae464644d847dc9ae3
Binary Wang <binarywang@gmail.com>
2017-12-16 15:23:48 +0800

    remove useless javadoc

--- weixin-java-common/src/main/java/me/chanjar/weixin/common/util/json/GsonHelper.java
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/util/json/GsonHelper.java
@@ -1,13 +1,4 @@
-/*
- * KINGSTAR MEDIA SOLUTIONS Co.,LTD. Copyright c 2005-2013. All rights reserved.
- *
- * This source code is the property of KINGSTAR MEDIA SOLUTIONS LTD. It is intended
- * only for the use of KINGSTAR MEDIA application development. Reengineering, reproduction
- * arose from modification of the original source, or other redistribution of this source
- * is not permitted without written permission of the KINGSTAR MEDIA SOLUTIONS LTD.
- */
 package me.chanjar.weixin.common.util.json;
-
 
 import com.google.common.collect.Lists;
 import com.google.gson.JsonArray;
@@ -15,7 +6,6 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.util.List;
-
 
 public class GsonHelper {
 
@@ -95,7 +85,7 @@ public class GsonHelper {
 
   public static boolean getAsPrimitiveBool(JsonElement element) {
     Boolean r = getAsBoolean(element);
-    return r != null && r.booleanValue();
+    return r != null && r;
   }
 
   public static Double getAsDouble(JsonElement element) {
@@ -129,6 +119,7 @@ public class GsonHelper {
 
     return result.toArray(new Integer[0]);
   }
+
   public static String[] getStringArray(JsonObject o, String string) {
     JsonArray jsonArray = getAsJsonArray(o.getAsJsonArray(string));
     if (jsonArray == null) {
--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/util/json/WxMaKefuMessageGsonAdapter.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/util/json/WxMaKefuMessageGsonAdapter.java
@@ -1,11 +1,3 @@
-/*
- * KINGSTAR MEDIA SOLUTIONS Co.,LTD. Copyright c 2005-2013. All rights reserved.
- *
- * This source code is the property of KINGSTAR MEDIA SOLUTIONS LTD. It is intended
- * only for the use of KINGSTAR MEDIA application development. Reengineering, reproduction
- * arose from modification of the original source, or other redistribution of this source
- * is not permitted without written permission of the KINGSTAR MEDIA SOLUTIONS LTD.
- */
 package cn.binarywang.wx.miniapp.util.json;
 
 import cn.binarywang.wx.miniapp.bean.WxMaKefuMessage;
