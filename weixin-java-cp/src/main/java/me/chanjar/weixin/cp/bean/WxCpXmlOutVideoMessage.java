0b7c1da5a84592f5ad869a322ee39f83099df848
Binary Wang <binarywang@gmail.com>
2018-03-03 17:09:39 +0800

    fix compile warning

--- weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/WxCpXmlOutVideoMessage.java
+++ weixin-java-cp/src/main/java/me/chanjar/weixin/cp/bean/WxCpXmlOutVideoMessage.java
@@ -3,11 +3,13 @@ package me.chanjar.weixin.cp.bean;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamConverter;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.util.xml.XStreamCDataConverter;
 
 @XStreamAlias("xml")
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class WxCpXmlOutVideoMessage extends WxCpXmlOutMessage {
   private static final long serialVersionUID = -8672761162722733622L;
 
