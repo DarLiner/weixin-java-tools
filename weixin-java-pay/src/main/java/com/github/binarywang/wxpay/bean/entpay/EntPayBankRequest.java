dce4f6fb0679e8e2ccef3ae21e6051c5b21f9b58
Binary Wang <binarywang@gmail.com>
2018-04-10 14:22:10 +0800

    修复代码

--- weixin-java-pay/src/main/java/com/github/binarywang/wxpay/bean/entpay/EntPayBankRequest.java
+++ weixin-java-pay/src/main/java/com/github/binarywang/wxpay/bean/entpay/EntPayBankRequest.java
@@ -3,6 +3,7 @@ package com.github.binarywang.wxpay.bean.entpay;
 import com.github.binarywang.wxpay.bean.request.BaseWxPayRequest;
 import com.github.binarywang.wxpay.exception.WxPayException;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,6 +21,7 @@ import me.chanjar.weixin.common.annotation.Required;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @XStreamAlias("xml")
 public class EntPayBankRequest extends BaseWxPayRequest {
@@ -32,6 +34,7 @@ public class EntPayBankRequest extends BaseWxPayRequest {
    * 示例值：1212121221227
    * 类型：string(32)
    * 描述：商户订单号，需保持唯一（只允许数字[0~9]或字母[A~Z]和[a~z]，最短8位，最长32位）
+   * </pre>
    */
   @Required
   @XStreamAlias("partner_trade_no")
@@ -46,7 +49,7 @@ public class EntPayBankRequest extends BaseWxPayRequest {
    * 示例值：8609cb22e1774a50a930e414cc71eca06121bcd266335cda230d24a7886a8d9f
    * 类型：string(64)
    * 描述：收款方银行卡号（采用标准RSA算法，公钥由微信侧提供）,详见获取RSA加密公钥API
-   *
+   * </pre>
    */
   @Required
   @XStreamAlias("enc_bank_no")
@@ -61,6 +64,7 @@ public class EntPayBankRequest extends BaseWxPayRequest {
    * 示例值：ca775af5f841bdf424b2e6eb86a6e21e
    * 类型：string(64)
    * 描述：收款方用户名（采用标准RSA算法，公钥由微信侧提供）详见获取RSA加密公钥API
+   * </pre>
    */
   @Required
   @XStreamAlias("enc_true_name")
@@ -74,6 +78,7 @@ public class EntPayBankRequest extends BaseWxPayRequest {
    * 示例值：1001
    * 类型：string(64)
    * 描述：银行卡所在开户行编号,详见银行编号列表
+   * </pre>
    */
   @Required
   @XStreamAlias("bank_code")
@@ -87,6 +92,7 @@ public class EntPayBankRequest extends BaseWxPayRequest {
    * 示例值：100000
    * 类型：int
    * 描述：付款金额：RMB分（支付总额，不含手续费） 注：大于0的整数
+   * </pre>
    */
   @Required
   @XStreamAlias("amount")
@@ -100,6 +106,7 @@ public class EntPayBankRequest extends BaseWxPayRequest {
    * 示例值：理财
    * 类型：string
    * 描述：企业付款到银行卡付款说明,即订单备注（UTF8编码，允许100个字符以内）
+   * </pre>
    */
   @Required
   @XStreamAlias("desc")
