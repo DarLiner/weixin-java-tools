b901f731073693bebc5dfd5c4cf4fc299e1768d1
Binary Wang <binarywang@gmail.com>
2018-06-09 23:59:27 +0800

    clean code

--- weixin-java-common/src/main/java/me/chanjar/weixin/common/util/http/RequestExecutor.java
+++ weixin-java-common/src/main/java/me/chanjar/weixin/common/util/http/RequestExecutor.java
@@ -9,51 +9,13 @@ import java.io.IOException;
  *
  * @param <T> 返回值类型
  * @param <E> 请求参数类型
+ * @author Daniel Qian
  */
 public interface RequestExecutor<T, E> {
 
   /**
    * @param uri  uri
    * @param data 数据
-   * @throws WxErrorException
-   * @throws IOException
    */
   T execute(String uri, E data) throws WxErrorException, IOException;
-
-  /**
-   * apache-http实现方式
-   * @param httpclient
-   * @param httpProxy
-   * @param uri
-   * @param data
-   * @return
-   * @throws WxErrorException
-   * @throws IOException
-   *//*
-  T executeApache(CloseableHttpClient httpclient, HttpHost httpProxy, String uri, E data) throws WxErrorException, IOException;
-
-    *//**
-   * jodd-http实现方式
-   * @param provider
-   * @param proxyInfo
-   * @param uri
-   * @param data
-   * @return
-   * @throws WxErrorException
-   * @throws IOException
-   *//*
-  T executeJodd(HttpConnectionProvider provider, ProxyInfo proxyInfo, String uri, E data) throws WxErrorException, IOException;
-
-
-  *//** okhttp实现方式
-   * @param pool
-   * @param proxyInfo
-   * @param uri
-   * @param data
-   * @return
-   * @throws WxErrorException
-   * @throws IOException
-   *//*
-  T executeOkhttp(ConnectionPool pool, final OkHttpProxyInfo proxyInfo, String uri, E data) throws WxErrorException, IOException;
-*/
 }
